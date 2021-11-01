1200940c32b3a28c90bd0d620e4b0630b1f186fc
Les Lakewood <llakewood@opentext.com>
2021-11-01 12:21:13 -0400

    initial fork of callback bot

--- README.md
+++ README.md
@@ -1,1 +1,175 @@
-# oas-unlock-bot
+# oas-callback-Bot
+
+## Summary
+
+Bot Framework v4 application.
+
+This bot has been created using [Bot Framework](https://dev.botframework.com), it shows how to create a simple bot that accepts input from the user and echoes it back.
+
+The project is building a chatbot to help OAS user to understand and know their pension information. to be add more when we have a clear requirement in the future
+
+## Build Status
+
+<a href="https://teamcity.dts-stn.com/viewType.html?buildTypeId=OasUnlockBot_DeployBdmDev&guest=1" >
+<img src="https://teamcity.dts-stn.com/app/rest/builds/buildType:(id:5076)/statusIcon"/>
+</a>
+
+## Table of Contents
+
+- [oas-callback-Bot](#oas-unblock-bot)
+  - [Summary](#summary)
+  - [Table of Contents](#table-of-contents)
+  - [Prerequisites](#prerequisites)
+  - [Technology stack](#technology-stack)
+  - [Installation](#installation)
+    - [Running the project with docker](#running-the-project-with-docker)
+    - [Running the project with npm](#running-the-project-with-npm)
+  - [Database and storage](#database-and-storage)
+  - [Testing](#testing)
+    - [Unit testing](#unit-testing)
+    - [Integration Testing](#integration-testing)
+  - [Testing the bot using Bot Framework Emulator](#testing-the-bot-using-bot-framework-emulator)
+    - [Connect to the bot using Bot Framework Emulator](#connect-to-the-bot-using-bot-framework-emulator)
+  - [Interacting with the bot](#interacting-with-the-bot)
+  - [Deploy the bot to Azure](#deploy-the-bot-to-azure)
+  - [Pipeline Integration](#pipeline-integration)
+  - [Further reading](#further-reading)
+  - [Config Changelog](#config-changelog)
+
+## Prerequisites
+
+- [Node.js](https://nodejs.org) version 15.0 or higher
+
+  ```bash
+  # determine node version
+  node --version
+  ```
+
+- [npm](https://www.npmjs.com/)
+- [docker](https://docs.docker.com/get-docker/)
+- [docker-compose](https://docs.docker.com/compose/install/)
+- [bot-framework](https://dev.botframework.com/)
+
+## Technology stack
+
+- [restify](http://restify.com/) - Backend framework
+- [TypeScript](https://www.typescriptlang.org/) - Backend programming language
+- [Azure Cloud](https://www.azure.com) - Cloud provider
+- [Data storage](TBD) - TBD
+
+## Installation
+
+Follow these steps to get started.
+
+- Clone the repository
+
+```bash
+git clone https://github.com/DTS-STN/oas-callback-bot
+```
+
+or
+
+```back
+git clone git@github.com:DTS-STN/oas-callback-bot.git
+```
+
+### Running the project with docker
+
+1. Add a `.env.local` file to `.config`
+
+- `cp .config/.env.example .config/.env.local`
+- Update `.config/.env.local` with variables provided by existing dev.
+
+To run the server
+
+```bash
+make run-local-development
+```
+
+To tear down your environment:
+
+```bash
+make close-local
+```
+
+Workspaces:
+
+- Server - `make local-server-workspace`
+
+### Running the project with npm
+
+- Add a `.env` file to the directory root
+- `cp config/.env.example /.env`
+- Update `/.env` with variables provided by existing dev.
+- Install modules
+
+  ```bash
+  npm install
+  ```
+
+- Start the bot
+
+  ```bash
+  npm run start:dev
+  ```
+
+## Database and storage
+
+- TBD
+
+## Testing
+
+### Unit testing
+
+- will using bot framework testing client to do the unit test. for more information please visit [bot doc](https://docs.microsoft.com/en-us/azure/bot-service/unit-test-bots?view=azure-bot-service-4.0&tabs=csharp)
+
+### Integration Testing
+
+- TBD
+
+## Testing the bot using Bot Framework Emulator
+
+[Bot Framework Emulator](https://github.com/microsoft/botframework-emulator) is a desktop application that allows bot developers to test and debug their bots on localhost or running remotely through a tunnel.
+
+- Install the latest Bot Framework Emulator from [here](https://github.com/Microsoft/BotFramework-Emulator/releases)
+
+### Connect to the bot using Bot Framework Emulator
+
+- Launch Bot Framework Emulator
+- File -> Open Bot
+- Enter a Bot URL of `http://localhost:3978/api/messages`
+- Enter a Dev Bot URL `https://dts-oas-callback-bot-dev.bdm-dev.dts-stn.com/api/messages`
+- if you are using docker container or connect to dev env from your local laptop, please download ngrok and config as below ![config-ngrok](config_ngrok.png)
+- otherwise do not need this step
+- Enter MicrosoftAppId and MicrosoftAppPassword from your .env file
+
+## Interacting with the bot
+
+Enter text in the emulator. The text will be echoed back by the bot.
+
+## Deploy the bot to Azure
+
+To learn more about deploying a bot to Azure, see [Deploy your bot to Azure](https://aka.ms/azuredeployment) for a complete list of deployment instructions.
+
+## Pipeline Integration
+
+This application leverages GitHub actions and TeamCity Pipelines for performing pre-merge regression testing.
+On PR creation or update, the Pipelines will run the entire API test collection, as well as the integration tests.
+The github action will run testing and build docker container for application. the TeamCity will using helm tool that deploy the container image to k8s cluster.
+
+## Further reading
+
+- [Bot Framework Documentation](https://docs.botframework.com)
+- [Bot Basics](https://docs.microsoft.com/azure/bot-service/bot-builder-basics?view=azure-bot-service-4.0)
+- [Activity processing](https://docs.microsoft.com/en-us/azure/bot-service/bot-builder-concept-activity-processing?view=azure-bot-service-4.0)
+- [Azure Bot Service Introduction](https://docs.microsoft.com/azure/bot-service/bot-service-overview-introduction?view=azure-bot-service-4.0)
+- [Azure Bot Service Documentation](https://docs.microsoft.com/azure/bot-service/?view=azure-bot-service-4.0)
+- [Azure CLI](https://docs.microsoft.com/cli/azure/?view=azure-cli-latest)
+- [Azure Portal](https://portal.azure.com)
+- [Channels and Bot Connector Service](https://docs.microsoft.com/en-us/azure/bot-service/bot-concepts?view=azure-bot-service-4.0)
+- [Restify](https://www.npmjs.com/package/restify)
+- [dotenv](https://www.npmjs.com/package/dotenv)
+
+## Config Changelog
+
+- None
---
+++ src/dialogs/confirmCallbackStep.ts
@@ -0,0 +1,221 @@
+import {
+  TextPrompt,
+  ComponentDialog,
+  WaterfallDialog,
+  ChoiceFactory,
+  WaterfallStepContext,
+} from 'botbuilder-dialogs';
+
+import { LuisRecognizer } from 'botbuilder-ai';
+
+import i18n from './locales/i18nConfig';
+import { CallbackBotDetails } from './callbackBotDetails';
+import { CallbackRecognizer } from './calllbackDialogs/callbackRecognizer';
+
+const TEXT_PROMPT = 'TEXT_PROMPT';
+export const CONFIRM_CALLBACK_STEP = 'CONFIRM_CALLBACK_STEP';
+const CONFIRM_CALLBACK_WATERFALL_STEP = 'CONFIRM_CALLBACK_WATERFALL_STEP';
+
+const MAX_ERROR_COUNT = 3;
+
+export class ConfirmCallbackStep extends ComponentDialog {
+  constructor() {
+    super(CONFIRM_CALLBACK_STEP);
+
+    // Add a text prompt to the dialog stack
+    this.addDialog(new TextPrompt(TEXT_PROMPT));
+
+    this.addDialog(
+      new WaterfallDialog(CONFIRM_CALLBACK_WATERFALL_STEP, [
+        this.preStep.bind(this),
+        // this.initialStep.bind(this),
+        this.finalStep.bind(this),
+      ]),
+    );
+
+    this.initialDialogId = CONFIRM_CALLBACK_WATERFALL_STEP;
+  }
+  async preStep(stepContext: WaterfallStepContext) {
+    // Get the user details / state machine
+    const callbackBotDetails: CallbackBotDetails =
+      stepContext.options as CallbackBotDetails;
+
+    // Set the text for the prompt
+    const okMsg = i18n.__('OKMsg');
+    const standardMsg = i18n.__('callbackBotDialogStepStandardMsg');
+
+    // Set the text for the retry prompt
+    const retryMsg = i18n.__('confirmCallbackStepRetryMsg');
+
+    // Check if the error count is greater than the max threshold
+    if (callbackBotDetails.errorCount.confirmCallbackStep >= MAX_ERROR_COUNT) {
+      // Throw the master error flag
+      callbackBotDetails.masterError = true;
+
+      // Set master error message to send
+      const errorMsg = i18n.__('confirmCallbackStepErrorMsg');
+
+      // Send master error message
+      await stepContext.context.sendActivity(errorMsg);
+
+      // End the dialog and pass the updated details state machine
+      return await stepContext.endDialog(callbackBotDetails);
+    }
+
+    // Check the user state to see if callbackBotDetails.confirmCallbackStep is set to null or -1
+    // If it is in the error state (-1) or or is set to null prompt the user
+    // If it is false the user does not want to proceed
+    if (
+      callbackBotDetails.confirmCallbackStep === null ||
+      callbackBotDetails.confirmCallbackStep === -1
+    ) {
+      // Setup the prompt message
+      let promptMsg = standardMsg;
+
+      // The current step is an error state
+      if (callbackBotDetails.confirmCallbackStep === -1) {
+        promptMsg = retryMsg;
+      }
+      await stepContext.context.sendActivity(okMsg);
+      const promptOptions: any = i18n.__(
+        'confirmCallbackStandardPromptOptions',
+      );
+      const promptDetails = {
+        prompt: ChoiceFactory.forChannel(
+          stepContext.context,
+          promptOptions,
+          promptMsg,
+        ),
+      };
+
+      return await stepContext.prompt(TEXT_PROMPT, promptDetails);
+    } else {
+      return await stepContext.next(false);
+    }
+  }
+  /**
+   * Initial step in the waterfall. This will kick of the ConfirmCallbackStep step
+   *
+   * If the confirmCallbackStep flag is set in the state machine then we can just
+   * end this whole dialog
+   *
+   * If the confirmCallbackStep flag is set to null then we need to get a response from the user
+   *
+   * If the user errors out then we're going to set the flag to false and assume they can't / don't
+   * want to proceed
+   */
+  async initialStep(stepContext) {
+    // Get the user details / state machine
+    const callbackBotDetails = stepContext.options;
+
+    // Set the text for the prompt
+    const standardMsg = i18n.__('confirmCallbackStandardMsg');
+
+    // Set the text for the retry prompt
+    const retryMsg = i18n.__('confirmLookIntoStepRetryMsg');
+
+    // Check if the error count is greater than the max threshold
+    if (callbackBotDetails.errorCount.confirmCallbackStep >= MAX_ERROR_COUNT) {
+      // Throw the master error flag
+      callbackBotDetails.masterError = true;
+
+      // Set master error message to send
+      const errorMsg = i18n.__('confirmCallbackStepErrorMsg');
+
+      // Send master error message
+      await stepContext.context.sendActivity(errorMsg);
+
+      // End the dialog and pass the updated details state machine
+      return await stepContext.endDialog(callbackBotDetails);
+    }
+
+    // Check the user state to see if callbackBotDetails.confirmCallbackStep is set to null or -1
+    // If it is in the error state (-1) or or is set to null prompt the user
+    // If it is false the user does not want to proceed
+    if (
+      callbackBotDetails.confirmCallbackStep === null ||
+      callbackBotDetails.confirmCallbackStep === -1
+    ) {
+      // Setup the prompt message
+      let promptMsg = standardMsg;
+
+      // The current step is an error state
+      if (callbackBotDetails.confirmCallbackStep === -1) {
+        promptMsg = retryMsg;
+      }
+
+      return await stepContext.prompt(TEXT_PROMPT, promptMsg);
+    } else {
+      return await stepContext.next(false);
+    }
+  }
+
+  /**
+   * Validation step in the waterfall.
+   * We use LUIZ to process the prompt reply and then
+   * update the state machine (callbackBotDetails)
+   */
+  async finalStep(stepContext) {
+    // Get the user details / state machine
+    const callbackDetails = stepContext.options;
+    let luisRecognizer;
+    let lang = 'en';
+    // Language check
+
+    // Then change LUIZ appID
+    if (
+      stepContext.context.activity.locale.toLowerCase() === 'fr-ca' ||
+      stepContext.context.activity.locale.toLowerCase() === 'fr-fr'
+    ) {
+      lang = 'fr';
+    }
+
+    // LUIZ Recogniser processing
+    luisRecognizer = new CallbackRecognizer(lang);
+    // Call prompts recognizer
+    const recognizerResult = await luisRecognizer.executeLuisQuery(
+      stepContext.context,
+    );
+
+    // Top intent tell us which cognitive service to use.
+    const intent = LuisRecognizer.topIntent(recognizerResult, 'None', 0.5);
+
+    const closeMsg = i18n.__('confirmCallbackStepCloseMsg');
+
+    switch (intent) {
+      // Proceed
+      case 'promptConfirmYes':
+        console.log('INTENT: ', intent);
+        callbackDetails.confirmCallbackStep = true;
+        const firstWelcomeMsg = i18n.__('getCallbackScheduleStandardMsg');
+        const standardMsgContinue = i18n.__('confirmAuthStepMsg');
+        const confirmationCodeMsg = i18n.__('confirmAuthWordStepStandardMsg');
+        await stepContext.context.sendActivity(firstWelcomeMsg);
+        await stepContext.context.sendActivity(standardMsgContinue);
+        await stepContext.context.sendActivity(confirmationCodeMsg);
+        return await stepContext.endDialog(callbackDetails);
+
+      // Don't Proceed
+      case 'promptConfirmNo':
+        console.log('INTENT: ', intent);
+
+        await stepContext.context.sendActivity(closeMsg);
+
+        callbackDetails.confirmCallbackStep = false;
+        return await stepContext.endDialog(callbackDetails);
+
+      // Could not understand / None intent
+      default: {
+        // Catch all
+        console.log('NONE INTENT');
+        callbackDetails.confirmLookIntoStep = -1;
+        callbackDetails.errorCount.confirmLookIntoStep++;
+
+        return await stepContext.replaceDialog(
+          CONFIRM_CALLBACK_STEP,
+          callbackDetails,
+        );
+      }
+    }
+  }
+}
