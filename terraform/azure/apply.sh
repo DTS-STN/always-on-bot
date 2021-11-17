f8fbebb7c55a845d7308575222d768c567152d05
Ben Larabie <benlarabie@gmail.com>
2021-11-17 14:37:41 -0500

    TF debug

--- terraform/azure/apply.sh
+++ terraform/azure/apply.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 mkdir -p /workspace
 cd /workspace
-cp -r %system.teamcity.build.checkoutDir%/terraform/azure .
+cp -r $WORKSPACE/terraform/azure .
 cd azure
-export LC_ALL=en_US.UTF-8
-az login --service-principal -u %TEAMCITY_USER% -p %TEAMCITY_PASS% --tenant %env.TENANT-ID%
 terraform init
 terraform plan
