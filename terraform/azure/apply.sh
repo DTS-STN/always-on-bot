8bba8d12e1ca01578f0674df9a35661722d6ecb9
Ben Larabie <benlarabie@gmail.com>
2021-11-19 10:31:49 -0500

    Removing debug, adding auto approve apply

--- terraform/azure/apply.sh
+++ terraform/azure/apply.sh
@@ -3,9 +3,5 @@ mkdir -p /workspace
 cd /workspace
 cp -r $WORKSPACE/terraform/azure .
 cd azure
-echo $ARM_CLIENT_ID
-echo $ARM_CLIENT_SECRET
-echo $ARM_SUBSCRIPTION_ID
-echo $ARM_TENANT_ID
 terraform init
-terraform plan
+terraform apply -auto-approve
