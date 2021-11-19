5c1948a2bf4bbc6e353827cfaeeb4ef2266e026c
Ben Larabie <benlarabie@gmail.com>
2021-11-19 09:43:19 -0500

    Adding debug to script

--- terraform/azure/apply.sh
+++ terraform/azure/apply.sh
@@ -3,5 +3,9 @@ mkdir -p /workspace
 cd /workspace
 cp -r $WORKSPACE/terraform/azure .
 cd azure
+echo $ARM_CLIENT_ID
+echo $ARM_CLIENT_SECRET
+echo $ARM_SUBSCRIPTION_ID
+echo $ARM_TENANT_ID
 terraform init
 terraform plan
