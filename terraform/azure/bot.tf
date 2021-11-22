2f63724e4c58654fb7f4ce8b13036cd3ce10e894
Les Lakewood <llakewood@opentext.com>
2021-11-22 11:49:24 -0500

    azurerm_resource_group.oas_unblock_luis.name

--- terraform/azure/bot.tf
+++ terraform/azure/bot.tf
@@ -5,7 +5,7 @@ data "azurerm_client_config" "current" {}
 resource "azurerm_bot_channels_registration" "oas_unblock_bot_bdm_dev" {
   name                = "oas_unblock_bot_bdm_dev"
   location            = "global"
-  resource_group_name = var.resource_group_name
+  resource_group_name = var.azurerm_resource_group.oas_unblock_luis.name
   sku                 = "F0"
   microsoft_app_id    = "${data.azurerm_client_config.current.client_id}"
 }
