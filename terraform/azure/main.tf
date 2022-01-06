5e743c80273fc17a6fe8de49a3731a86b3d4933e
Les Lakewood <llakewood@opentext.com>
2022-01-06 13:20:33 -0500

    adding TF_VAR to main.tf

--- terraform/azure/main.tf
+++ terraform/azure/main.tf
@@ -6,20 +6,20 @@ resource "azurerm_resource_group" "virtual_assistant_luis" {
 #LUIS
 resource "azurerm_cognitive_account" "luis_virtual_assistant" {
   name                = "${var.luis_instance_name}"
-  location            = azurerm_resource_group.virtual_assistant_luis.location
+  location            = "${var.luis_virtual_assistant_location}"
   resource_group_name = "${var.resource_group_name}"
   kind                = "LUIS"
 
   sku_name = var.luis_sku_name
   tags = {
-"Environment" = "Dev"
-"CostCenter" = "ML/AI"
-}
+  "Environment" = "Dev"
+  "CostCenter" = "ML/AI"
+  }
 }
 
-resource "azurerm_cognitive_account" "luis_virtual_assistant-authoring" {
+resource "azurerm_cognitive_account" "luis_virtual_assistant_authoring" {
   name                = "${var.luis_authoring_instance_name}"
-  location            = azurerm_resource_group.virtual_assistant_luis.location
+  location            = "${var.luis_virtual_assistant_authoring_location}"
   resource_group_name = "${var.resource_group_name}"
   kind                = "LUIS.Authoring"
   #Authoring only has one SKU tier
--- terraform/azure/variables.tf
+++ terraform/azure/variables.tf
@@ -1,5 +1,9 @@
+# declare the variables, set default values as needed
+
 variable "luis_instance_name" {}
 variable "luis_sku_name" {}
 variable "location" {}
 variable "resource_group_name" {}
 variable "luis_authoring_instance_name" {}
+variable "luis_virtual_assistant_location" {}
+variable "luis_virtual_assistant_authoring_location" {}
